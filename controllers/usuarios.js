12643053c6466cacb67a14a43ba6344231be5ee1
Javier Echavez Chardaux <javier.echavez@ludycom.com>
2022-01-05 13:40:19 -0500

    prueba 2 en heroku promise catch

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -8,18 +8,21 @@ const usuariosGet = async (req = request, res = response) => {
     const { limite = 5, desde = 0 } = req.query;
     const query = { estado: true };
 
-    const usuarios = await Usuario.find(query)
-        .skip(desde)
-        .limit(limite);
+    // const usuarios = await Usuario.find(query)
+    //     .skip(desde)
+    //     .limit(limite);
 
-    const total = await Usuario.countDocuments(query);
+    // const total = await Usuario.countDocuments(query);
 
-    // const [total, usuarios] = await Promise.all([
-    //     Usuario.countDocuments(query),
-    //     Usuario.find(query)
-    //         .skip(desde)
-    //         .limit(limite)
-    // ])
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(desde)
+            .limit(limite)
+    ]).catch((err) => {
+        console.log(err);
+        return
+    })
 
     res.json({
         total,
