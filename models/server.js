1c6c5ba7df9a7d265510f4ac1bcd4c31a145296f
Javier Echavez Chardaux <javier.echavez@ludycom.com>
2022-01-12 10:27:36 -0500

    CRUD Finalizado con busqueda general

--- models/server.js
+++ models/server.js
@@ -9,9 +9,10 @@ class Server {
         this.port = process.env.PORT;
         this.paths = {
             auth: '/api/auth',
-            usuarios: '/api/usuarios',
+            buscar: '/api/buscar',
             categorias: '/api/categorias',
-            productos: '/api/productos'
+            productos: '/api/productos',
+            usuarios: '/api/usuarios',
         }
 
         // Conectar a base de datos
@@ -42,10 +43,11 @@ class Server {
     routes() {
 
         this.app.use(this.paths.auth, require('../routes/auth'));
-        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.productos, require('../routes/productos'));
-        
+        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
+
     }
 
     listen() {
